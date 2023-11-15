656b6e3e5740ba8d24478929193be05afab5bc02
Ahmed Hanye <ahmedhanye161004@gmail.com>
2023-11-15 18:52:03 +0200

    Update Youtube_Downloader.py

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,12 +1,25 @@
 from pytube import YouTube
 
 
-def Download(link):
-    yt = YouTube(link)
+def Download():
+    # Check if the link is valid and get the video
+    while True:
+        link = input("Enter the link of the video: ")
+        try:
+            yt = YouTube(link)
+        except:
+            print("\033[31mPlease Enter A Valid Link\033[0m")
+            continue
+        else:
+            break
     # Get the streams
     while True:
-        choice1 = int(input(
-            "Do you want to download the video or audio or both?\n1. Video\n2. Audio\n3. Both\nEnter the number of your choice: "))
+        try:
+            choice1 = int(input(
+                "Do you want to download the video or audio or both?\n1. Video\n2. Audio\n3. Both\nEnter the number of your choice: "))
+        except:
+            print("\033[31mPlease Enter A Valid Number\033[0m")
+            continue
         if choice1 == 1:
             # Get streams that only have video
             streams = yt.streams.filter(only_video=True)
@@ -19,6 +32,9 @@ def Download(link):
             # Get streams that include both video and audio
             streams = yt.streams.filter(progressive=True, file_extension='mp4')
             break
+        else:
+            print("\033[31mPlease Enter A Valid Number\033[0m")
+            continue
     # Print the streams
     l = len(streams)
     for i in range(l):
@@ -26,14 +42,21 @@ def Download(link):
         print(f"{i}: {stream}")
     # Get the choice of the stream
     while True:
-        choice2 = int(
-            input("Enter the number of the stream you want to download: "))
+        try:
+            choice2 = int(
+                input("Enter the number of the stream you want to download: "))
+        except:
+            print("\033[31mPlease Enter A Valid Number\033[0m")
+            continue
         if choice2 in range(l):
             break
+        else:
+            print("\033[31mPlease Enter A Valid Number\033[0m")
+            continue
     # Get the title and URL of the stream
     title = yt.title
     URL = streams[choice2].url
     print(f"\n\n\nname: {title}\n\nDownload URL: {URL}\n")
 
 
-Download(input("Enter the link: "))
+Download()
